c9f8072a59438ac2b309cebb270d91902e5d0bcc
Vic Chimenti <chimenti@seattleu.edu>
2019-05-13 16:54:40 -0700

    ready unless we want to remove user input

--- cyclomatic.py
+++ cyclomatic.py
@@ -1,38 +1,9 @@
 import datetime
 
 
-# function for validating user input is a valid date in the requested format
-def validate_time(date_in):
-    try:
-        datetime.datetime.strptime(date_in, '%y-%m-%d')
-    except ValueError:
-        print('Invalid Date Format, enter format YY-MM-DD')
-        return False
-    print("You entered a valid date")
-    return True
-
-
-# function for gathering user input
-def get_user_input():
-    # Option for User Input
-    date_input = input("Enter any month and day in the format YY-MM-DD: ")
-    # *** IF STATEMENT ADDS 1 to Cyclomatic Complexity SCORE  ***   #
-    if validate_time(date_input) is True:
-        return date_input
-    else:
-        # Stub user input if user does not input a valid date
-        date_input = stub_user_input()
-        return date_input
-
-
-# stub user input function
-def stub_user_input():
-    # return a hard-coded valid date
-    return "01-01-01"
-
-
 # function for generating the current timestamp
 def current_timestamp():
+    print("Printing the Current Time Stamp: ")
     date_today = datetime.datetime.now()
     print("Current Date/Time: ")
     return date_today
@@ -47,20 +18,27 @@ def switch(case):
         'A': 4,
         'B': 5,
         'C': 6,
+        'default': 10,
     }[case]
 
 
 #   ***   main driver   ***   Cyclomatic Complexity   ***   Total == 1   ***  #
+print("You will enter a letter which the program will use to map to a number.")
+print("That number will be used to determine how many time a for loop iterates.")
+input_validation_list = ['A', 'B', 'C', 'a', 'b', 'c']
 # get input for switch case
-case_input = str(input("Enter A, B, or C and we will convert it to an integer and print that many time stamps: "))
+case_input = str(input("Enter A, B, or C: "))
 
-# Call to switch adds 1 to Cyclomatic Complexity *** Total == 2   ***   #
-num_iterations = int((switch(case_input)))
+# validate user input with   ***   IF STATEMENT ADDS 1 *** Cyclomatic Complexity *** Total == 2   ***   #
+if case_input in input_validation_list:
+    # Call to switch adds 1 to Cyclomatic Complexity *** Total == 2   ***   #
+    num_iterations = int((switch(case_input)))
+else:
+    case_input = 'default'
+    # Call to switch adds 1 to Cyclomatic Complexity *** Total == 2   ***   #
+    num_iterations = int((switch(case_input)))
 
 #   *** FOR LOOP ADDS 1 to Cyclomatic Complexity ***   Total == 3   ***  #
 for i in range(num_iterations):
     # Get Current Timestamp
     print(str(current_timestamp()))
-
-# call get user input function adds 1 to Cyclomatic Complexity   *** Total == 4   ***   #
-print(str(get_user_input()))
