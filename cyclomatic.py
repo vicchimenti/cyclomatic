92912a38d5c13c63dd64804b073145c26e9d4ee5
Vic Chimenti <chimenti@seattleu.edu>
2019-05-13 13:15:05 -0700

    update

--- cyclomatic.py
+++ cyclomatic.py
@@ -16,7 +16,7 @@ def validate_time(date_in):
 def get_user_input():
     # Option for User Input
     date_input = input("Enter any month and day in the format YY-MM-DD: ")
-    # *** IF STATEMENT ADDS 1 to Cyclomatic Complexity
+    # *** IF STATEMENT ADDS 1 to Cyclomatic Complexity SCORE  ***   #
     if validate_time(date_input) is True:
         return date_input
     else:
@@ -31,16 +31,6 @@ def stub_user_input():
     return "01-01-01"
 
 
-# mock the user input function
-def mock_user_input():
-    return stub_user_input()
-
-
-# mock object of user input
-def mock_object_user_input():
-    return mock_user_input()
-
-
 # function for generating the current timestamp
 def current_timestamp():
     date_today = datetime.datetime.now()
@@ -48,22 +38,29 @@ def current_timestamp():
     return date_today
 
 
-#   ***   main driver   ***   #
+# switch case function
+def switch(case):
+    return {
+        'a': 1,
+        'b': 2,
+        'c': 3,
+        'A': 4,
+        'B': 5,
+        'C': 6,
+    }[case]
 
-# Get Current Timestamp and Display Before Program Action
-print(str(current_timestamp()))
 
-# call get user input function
-user_input = get_user_input()
+#   ***   main driver   ***   Cyclomatic Complexity   ***   Total == 1   ***  #
+# get input for switch case
+case_input = str(input("Enter A, B, or C and we will convert it to an integer and print that many time stamps: "))
 
-# print the mock user input
-print(mock_user_input())
+# Call to switch adds 1 to Cyclomatic Complexity *** Total == 2   ***   #
+num_iterations = int((switch(case_input)))
 
-# print the mock object
-print(mock_object_user_input())
+#   *** FOR LOOP ADDS 1 to Cyclomatic Complexity ***   Total == 3   ***  #
+for i in range(num_iterations):
+    # Get Current Timestamp
+    print(str(current_timestamp()))
 
-# call validate time function
-print(str(validate_time(user_input)))
-
-# Get Updated Timestamp and Display After Program Action
-print(str(current_timestamp()))
+# call get user input function adds 1 to Cyclomatic Complexity   *** Total == 4   ***   #
+print(str(get_user_input()))
