5976ebe67857ac7a966a581d56465e537fdb524b
Victor Chimenti <chimenti@seattleu.edu>
2019-05-14 07:03:09 -0700

    updated remarks

--- cyclomatic.py
+++ cyclomatic.py
@@ -24,21 +24,21 @@ def switch(case):
 
 #   ***   main driver   ***   Cyclomatic Complexity   ***   Total == 1   ***  #
 print("You will enter a letter which the program will use to map to a number.")
-print("That number will be used to determine how many time a for loop iterates.")
+print("That number will be used to determine how many times a for loop iterates.")
+print("Each time the loop iterates it will print a current time stamp.")
 input_validation_list = ['A', 'B', 'C', 'a', 'b', 'c']
-# get input for switch case
+# get user input for switch case
 case_input = str(input("Enter A, B, or C: "))
 
-# validate user input with   ***   IF STATEMENT ADDS 1 *** Cyclomatic Complexity *** Total == 2   ***   #
+# validate user input with   ***   IF STATEMENT ADDS 1 to Cyclomatic Complexity *** Total == 2   ***   #
 if case_input in input_validation_list:
-    # Call to switch adds 1 to Cyclomatic Complexity *** Total == 2   ***   #
+    # Call to switch adds 1 to Cyclomatic Complexity *** Total == 3   ***   #
     num_iterations = int((switch(case_input)))
 else:
+    # if user did not enter valid input then assign the default case
     case_input = 'default'
-    # Call to switch adds 1 to Cyclomatic Complexity *** Total == 2   ***   #
+    # Call to switch adds 1 to Cyclomatic Complexity *** Total == 3   ***   #
     num_iterations = int((switch(case_input)))
 
-#   *** FOR LOOP ADDS 1 to Cyclomatic Complexity ***   Total == 3   ***  #
-for i in range(num_iterations):
-    # Get Current Timestamp
-    print(str(current_timestamp()))
+#   *** FOR LOOP ADDS 1 to Cyclomatic Complexity ***   Total == 4   ***  #
+for i in range(num_iterations): print(str(current_timestamp()))
